38f0eb662f7dbf94b41e99b04900cc6f094da6a0
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2026-01-01 20:27:27 -0500

    Add files via upload

---
+++ id.py
@@ -0,0 +1,13 @@
+a=[1,2,3,4]
+b=[1,2,3,4]
+c=a
+
+print("id of a is",id(a))
+print("id of b is",id(b))
+print("id of c is",id(c))
+print(a is b)
+print(a is not b)
+print(b is c)
+print(b is not c)
+print(a is c)
+print(a is not c)
