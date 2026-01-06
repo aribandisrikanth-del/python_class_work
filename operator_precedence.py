0b9542be6014b01226539f334f054af31ecf5400
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2026-01-05 20:19:38 -0500

    Add files via upload

---
+++ operator_precedence.py
@@ -0,0 +1,16 @@
+a=1
+b=2
+c=3
+d=4
+e=5
+
+f=a//e*(d**c)+12-8
+print("value of a//e(d**c)+12-8=",f,"\n")
+
+name="sunny"
+age=12
+
+if name=="sunny" or name=="sannigdh" and age <=13 and age>=18:
+    print("you are under 13 or above 18")
+else:
+    print("you are a teen")
