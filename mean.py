33a73f503958513e033ce33c690b35b84fb8d927
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2026-01-05 20:36:11 -0500

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,9 @@
+mean=38
+wrong_number=36
+correct_number=56
+total_number_of_numbers=40
+product=(mean+20)*total_number_of_numbers
+print("the correct sum of the 40 numbers =",product)
+
+mean2=product/total_number_of_numbers
+print("the correct mean of the 40 numbers =",mean2)
